53d225f8e82a224248450a906ed5afa48905226c
JunRain <sa2313@tukorea.ac.kr>
2023-03-06 11:39:38 +0900

    일대일 양방향

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -4,6 +4,7 @@ import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.concurrent.locks.Lock;
 
 @Entity
 public class Member {
@@ -19,6 +20,10 @@ public class Member {
     @ManyToOne
     @JoinColumn(insertable = false, updatable = false)
     private Team team;
+
+    @OneToOne
+    @JoinColumn(name = "LOCKER_ID") // 디폴트값이 지저분함
+    private Locker locker;
 
     public Long getId() {
         return id;
